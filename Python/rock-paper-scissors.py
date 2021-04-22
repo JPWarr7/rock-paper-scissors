6b7c4a9abc16b2cafb56844e8b161eeb3842bbe0
JPWarr7 <59145673+JPWarr7@users.noreply.github.com>
2021-04-22 16:13:44 -0400

    Add files via upload

--- Python/rock-paper-scissors.py
+++ Python/rock-paper-scissors.py
@@ -1,1 +1,38 @@
+def rock_paper_scissors():
 
+    import random
+
+    user = None
+
+    computer = random.choice(['rock', 'paper', 'scissors'])
+
+    while user == None:
+        user = input("Rock, paper, or scissors: ")
+        if user not in ['rock', 'paper', 'scissors']:
+            print("not a valid input!")
+            user = None
+    
+    print(f"You picked {user} and the computer picked {computer}.")
+    if user == computer:
+        return "Draw!"
+
+    if user == "rock" and computer == "scissors":
+        return "You win!"
+
+    if user == "rock" and computer == "paper":
+        return "You lose!"
+
+    if user == "scissors" and computer == "paper":
+        return "You win!"
+
+    if user == "scissors" and computer == "rock":
+        return "You lose!"
+
+    if user == "paper" and computer == "rock":
+        return "You win!"
+
+    if user == "paper" and computer == "scissors":
+        return "You lose!"
+
+
+rock_paper_scissors()
